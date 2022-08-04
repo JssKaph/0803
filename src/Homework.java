a1e379c564f2d914d8f324c265ad76b76feac6f2
JssKaph <tntmd2658@gmail.com>
2022-08-04 09:27:46 +0900

    0804 문제풀이

--- src/Homework.java
+++ src/Homework.java
@@ -1,13 +1,46 @@
+import java.util.Arrays;
+
 /*
  * 준과제
  * a = {{1,2},{3,4}}
  * b = {{5,6},{7,8}}
  * a+b = ? , a*b=?
  */
-
 public class Homework {
 	public static void main(String[] args) {
 		int[][] A = { { 1, 2 }, { 3, 4 } };
 		int[][] B = { { 5, 6 }, { 7, 8 } };
+		int[][] hapResults = new int[2][2];
+		int[][] gopResults = new int[2][2];
+		System.out.println("<<<행열의 합>>>");
+		matrixAdd(A, B, hapResults);
+		matrixPrint(hapResults);
+		System.out.println("<<<행열의 곱>>>");
+		matrixMultiple(A, B, gopResults);
+		matrixPrint(gopResults);
+	}
+
+	static void matrixMultiple(int[][] A, int[][] B, int[][] results) { // 행열의 곱
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A[i].length; j++) {
+				for (int k = 0; k < A[i].length; k++) { // 곱연산 (공식을 참고0
+					results[i][j] += A[i][k]*B[k][j];
+				}
+			}
+		}
+	}
+
+	static void matrixAdd(int[][] A, int[][] B, int[][] results) { // 행열의 합
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A[i].length; j++) {
+				results[i][j] = A[i][j] + B[i][j];
+			}
+		}
+	}
+
+	static void matrixPrint(int[][] results) { // 출력
+		for (int i = 0; i < results.length; i++) { // 밑으로
+			System.out.println(Arrays.toString(results[i])); // 1차원 배열 출력
+		}
 	}
 }
--- src/MethodOverloadingDemo.java
+++ src/MethodOverloadingDemo.java
@@ -1,4 +1,26 @@
-
+/*
+ * 메소드의 이름이 같을 때 그 메소드의 파라미터로 구별하는 기능
+ * 1) 메소드의 이름이 같아야 한다.
+ * 2) 반드시 메소드의 파라미터는 서로 달라야 한다.
+ * 3) 메소드의 리턴타입은 같거나 다르거나 상관없다.
+ */
 public class MethodOverloadingDemo {
+	public static void main(String[] args) {
+		System.out.println(add(34.5, 67.8));
+		System.out.println(add(4, 5, 6));
+		System.out.println(add(4, 5));
+	}
 
+	// Overloading, 중복정의
+	static int add(int a, int b) { // 메소드의 중복 적용, 파라미터가 다르기 때문에 add 라는 이름의 똑같은 메소드가 된다.
+		return a + b;
+	}
+
+	static int add(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	static double add(double a, double b) {
+		return a + b;
+	}
 }
